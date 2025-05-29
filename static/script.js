6bc2f5b587befdb45f0969dbdd0bcf0579405d64
Paulfox <ppauloa37@gmail.com>
2025-05-28 22:01:40 -0300

    Create script.js

---
+++ static/script.js
@@ -0,0 +1,12 @@
+    var wrapper = document.getElementById("wrapper")
+    var button = document.getElementById("acept")
+var cookie123 = function() {
+    var cookiedisplay = document.getElementById("cookie")
+    var cadastrar = localStorage.getItem("cadastrarpoint")
+    if (cadastrar === "securitycabe") {
+        var megasite = localStorage.setItem("aceitoAaEe", "codigosecuritycabe")
+    }
+    cookiedisplay.style.display = "none";
+    wrapper.style.marginTop = "70px";
+}
+    button.addEventListener("click", cookie123)
